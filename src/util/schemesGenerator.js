56f6903f575529b8868a57808826a205878f23f5
Joseph Surin <joseph.surin@gmail.com>
2019-06-25 12:54:45 +1000

    add circular scheme generator and overflow-y on all panes

--- src/util/schemesGenerator.js
+++ src/util/schemesGenerator.js
@@ -18,5 +18,36 @@ const analogous = (seed) => tc_generator(seed, 'analogous', byHue)
 const splitcomplement = (seed) => tc_generator(seed, 'splitcomplement')
 const triad = (seed) => tc_generator(seed, 'triad')
 const tetrad = (seed) => tc_generator(seed, 'tetrad')
+function circular(seed) {
+    var scheme = [tinycolor(seed)]
+    let { h, s, v } = tinycolor(seed).toHsv()
+    var sc = 100*s - 50
+    var vc = 100*v - 50
+    var hueshifts = [-15, -20, -35, 15, 20, 35]
+    var mods = [[-10, 0],[-20, -5], [-35, -10],[0, -10],[-5, -20],[-10, -35]]
+    var modpoints = [mods] // 1st quadrant
+    modpoints.push(mods.map(el => [-el[0], el[1]])) // 2nd quadrant
+    modpoints.push(modpoints[1].map(el => [el[0], -el[1]])) // 3rd quadrant
+    modpoints.push(mods.map(el => [el[0], -el[1]])) // 4th quadrant
+    var quadrant = 0
+    if(sc < 0 && vc > 0) {
+        quadrant = 1
+    } else if(sc < 0 && vc < 0) {
+        quadrant = 2
+    } else if(sc > 0 && vc < 0) {
+        quadrant = 3
+    }
+    modpoints[quadrant].forEach(([sm, vm], i) => {
+        var hn = (h + hueshifts[i]) % 361
+        if(hn < 0) {
+            hn += 360
+        }
+        var sn = (100*s + sm) / 100
+        var vn = (100*v + vm) / 100
+        scheme.push(tinycolor({ h: hn, s: sn, v: vn }))
+    })
+    scheme = scheme.sort(byBrightness).map(toHexString)
+    return ['circular', scheme]
+}
 
-export const schemeGenerators = [monochromatic, analogous, splitcomplement, triad, tetrad]
+export const schemeGenerators = [monochromatic, analogous, splitcomplement, triad, tetrad, circular]
